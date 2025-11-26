663ec53d2ead49596ad87902e1b14ab316c2ed12
Daniela Pereira Costa <1058912@senacminas.edu.br>
2025-11-26 16:13:19 -0300

    atividade9

---
+++ ex9.js
@@ -0,0 +1,18 @@
+const prompt = require('prompt-sync') ({sigint: true});
+
+// Array de produtos com nome e preço
+const produtos = [
+	{ produto: 'Lápis', preco: 2 },
+	{ produto: 'Mochila', preco: 80 },
+	{ produto: 'Caderno', preco: 15 },
+	
+];
+// Filtra apenas produtos com preço menor que 20
+const baratos = produtos.filter(p => p.preco < 20);
+
+if (baratos.length === 0) {
+	console.log('Nenhum produto com preço menor que 20 encontrado.');
+} else {
+	console.log('Produtos com preço menor que 20:');
+	baratos.forEach(p => console.log(`- ${p.produto}: R$ ${p.preco}`));
+}
